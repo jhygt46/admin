f2ad3adefbcd1e95f6b55a4b6faf73a5a256a0a9
jhygt46 <infolifeoperacional@gmail.com>
2017-05-22 18:48:20 -0400

    aa

--- ajax/ingreso.php
+++ ajax/ingreso.php
@@ -10,7 +10,7 @@ if($_SERVER['HTTP_HOST'] == "localhost"){
     $path = $_SERVER['DOCUMENT_ROOT']."admin/class";
 }
 
-require_once($path+"/mysql_class.php");
+//require_once($path+"/mysql_class.php");
 require_once($path+"/ingreso_class.php");
 $ingreso = new Ingreso();
 $info = $ingreso->ingresar_user();
