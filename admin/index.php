8a9e320db563ba8f71cf8e494828a133daabd334
jhygt46 <infolifeoperacional@gmail.com>
2017-05-22 12:18:40 -0400

    server

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,11 @@
 <?php
     session_start();
     
+    echo "<pre>";
+    print_r($_SERVER);
+    echo "</pre>";
+    
+    
     if(!isset($_SESSION['user']['info']['id_user'])){
         include("login.php");
     }else{
