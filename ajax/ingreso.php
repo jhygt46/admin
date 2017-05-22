e11eb7387a6e99486f684886c16fe01493858fed
jhygt46 <infolifeoperacional@gmail.com>
2017-05-22 18:49:48 -0400

    aa

--- ajax/ingreso.php
+++ ajax/ingreso.php
@@ -10,10 +10,12 @@ if($_SERVER['HTTP_HOST'] == "localhost"){
     $path = $_SERVER['DOCUMENT_ROOT']."admin/class";
 }
 
+echo $path+"/ingreso_class.php";
+
 //require_once($path+"/mysql_class.php");
-require_once($path+"/ingreso_class.php");
-$ingreso = new Ingreso();
-$info = $ingreso->ingresar_user();
-echo json_encode($info);
+//require_once($path+"/ingreso_class.php");
+//$ingreso = new Ingreso();
+//$info = $ingreso->ingresar_user();
+//echo json_encode($info);
 
 ?>
