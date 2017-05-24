90ce967e62024ba59e9742153121f089011941dc
jhygt46 <infolifeoperacional@gmail.com>
2017-05-24 14:07:53 -0400

    aa

--- class/ingreso_class.php
+++ class/ingreso_class.php
@@ -7,11 +7,13 @@ class Ingreso {
     public $con = null;
     
     public function __construct(){
-        //$this->con = new Conexion();
+        $this->con = new Conexion();
     }
     
     public function ingresar_user(){
-        
+        $aux['op'] = 1;
+        $aux['message'] = "OK";
+        return $aux;
     }
     
 }
