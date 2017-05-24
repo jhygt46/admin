f17eccb3b7f173cf0faecf4c3860202ed194e1b3
jhygt46 <infolifeoperacional@gmail.com>
2017-05-24 19:23:31 -0400

    aa

--- ajax/ingreso.php
+++ ajax/ingreso.php
@@ -5,12 +5,11 @@ header('Content-type: text/json');
 header('Content-type: application/json');
 
 if($_SERVER['HTTP_HOST'] == "localhost"){
-    $path = $_SERVER['DOCUMENT_ROOT'];
-    $path_ = $path."/admin/class";
+    $path = $_SERVER['DOCUMENT_ROOT']."/";
 }else{
     $path = $_SERVER['DOCUMENT_ROOT'];
-    $path_ = $path."admin/class";
 }
+$path_ = $path."admin/class";
 
 require_once($path_."/ingreso_class.php");
 //$ingreso = new Ingreso();
