669256b72cb881dfcd01697d9d8483e8014411c8
jhygt46 <infolifeoperacional@gmail.com>
2017-07-03 16:35:23 -0400

    aa

--- includes/info_nav.php
+++ includes/info_nav.php
@@ -1,28 +1,22 @@
 <?php
 
-$aux["ico"] = 4;
-$aux["categoria"] = "Productos";
-$aux["subcategoria"][0]["nombre"] = "Ingresar Categorias";
-$aux["subcategoria"][0]["link"] = "pages/crear_categoria.php";
-$aux["subcategoria"][1]["nombre"] = "Ingresar Productos";
-$aux["subcategoria"][1]["link"] = "pages/crear_producto.php";
-$menu[] = $aux; 
-unset($aux);
+    
+    $aux["ico"] = 2;
+    $aux["categoria"] = "Usuarios";
+    $aux["subcategoria"][0]["nombre"] = "Ingresar Usuarios";
+    $aux["subcategoria"][0]["link"] = "pages/crear_usuario.php";
+    
+    if($_SESSION['user']['info']['tareas'] == 1){
+    
+        $aux["subcategoria"][1]["nombre"] = "Ingresar Perfiles";
+        $aux["subcategoria"][1]["link"] = "pages/crear_perfil.php";
+    
+    }
+    
+    $menu[] = $aux;
+    unset($aux);
 
-$aux["ico"] = 2;
-$aux["categoria"] = "Usuarios";
-$aux["subcategoria"][0]["nombre"] = "Ingresar Usuarios";
-$aux["subcategoria"][0]["link"] = "pages/crear_usuario.php";
-$aux["subcategoria"][1]["nombre"] = "Ingresar Perfiles";
-$aux["subcategoria"][1]["link"] = "pages/crear_perfil.php";
-$menu[] = $aux;
-unset($aux);
+    
 
-$aux["ico"] = 1;
-$aux["categoria"] = "Configuracion";
-$aux["subcategoria"][0]["nombre"] = "Configuracion";
-$aux["subcategoria"][0]["link"] = "pages/configuracion.php";
-$menu[] = $aux;
-unset($aux);
 
 ?>
