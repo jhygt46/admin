9fa067486ff0c5df5400d03e80c7917abc85a2a8
jhygt46 <infolifeoperacional@gmail.com>
2017-07-03 20:28:07 -0400

    aa

--- pages/info_alumnos.php
+++ pages/info_alumnos.php
@@ -87,6 +87,10 @@ if(isset($_GET["id"]) && is_numeric($_GET["id"]) && $_GET["id"] != 0){
                         <input id="correo" type="text" value="<?php echo $that['correo']; ?>" require="" placeholder="diegomez13@hotmail.com" />
                         <div class="mensaje"></div>
                     </label>
+                    <ul class="papas clearfix" style="">
+                        <li style="float: left; width: 50%">PAPA</li>
+                        <li style="float: left; width: 50%">MAMA</li>
+                    </ul>
                     <label style='margin-top:20px'>
                         <span>&nbsp;</span>
                         <a id='button' onclick="form()">Enviar</a>
