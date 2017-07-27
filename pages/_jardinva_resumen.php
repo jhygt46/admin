d1cd1a75f0eefb0b2ea175d76c48c262ad989600
jhygt46 <infolifeoperacional@gmail.com>
2017-07-27 17:04:30 -0400

    aa

--- pages/_jardinva_resumen.php
+++ pages/_jardinva_resumen.php
@@ -171,7 +171,7 @@ if($tipo == 2){
         <?php for($i=0; $i<count($list); $i++){ $r=$i+1; if($i % 2 == 0){ $c = "color02"; }else{ $c = "color01"; } ?>
         <tr>
             <td><?php echo $r; ?></td>
-            <td align="left" style="padding: 2px 4px">- <?php echo $list[$i]['nombres']; ?> <?php echo $list[$i]['apellido_p']; ?></td>
+            <td align="left" style="padding: 2px 4px">- <?php echo $list[$i]['nombres']; ?> <?php echo utf8_encode($list[$i]['apellido_p']); ?></td>
             <?php for($m=1; $m<=31; $m++){ ?>
             <td></td>
             <?php } ?>
