d8fa53f383b16b4b391912be5a27cb041371913c
jhygt46 <infolifeoperacional@gmail.com>
2017-10-31 16:35:41 -0300

    aa

--- login.php
+++ login.php
@@ -8,7 +8,7 @@ include("includes/header.php");
         <tr>
             <td align='center' valign='middle'>
                 <div class='login'>
-                    <div class='titulo'></div>
+                    <div class='titulo'>PRUEBA</div>
                     <div class='contlogin'>
                         <div class='us'>
                             <div class='txt'>Correo</div>
