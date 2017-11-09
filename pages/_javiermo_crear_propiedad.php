e0ebf3699460b1f2a05cff67d5998fdf7f5e0b17
jhygt46 <infolifeoperacional@gmail.com>
2017-11-09 17:49:49 -0300

    aa

--- pages/_javiermo_crear_propiedad.php
+++ pages/_javiermo_crear_propiedad.php
@@ -47,30 +47,74 @@ if(isset($_GET["id"]) && is_numeric($_GET["id"]) && $_GET["id"] != 0){
     function initAutocomplete(){
         
         var map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: -33.8688, lng: 151.2195},
-            zoom: 13,
-            mapTypeId: 'roadmap'
+          center: {lat: -33.8688, lng: 151.2195},
+          zoom: 13,
+          mapTypeId: 'roadmap'
         });
 
+        // Create the search box and link it to the UI element.
         var input = document.getElementById('pac-input');
         var searchBox = new google.maps.places.SearchBox(input);
         map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
+        // Bias the SearchBox results towards current map's viewport.
         map.addListener('bounds_changed', function() {
-            searchBox.setBounds(map.getBounds());
+          searchBox.setBounds(map.getBounds());
         });
-        searchBox.addListener('places_changed', function(){
-            
-            var places = searchBox.getPlaces();
-            console.log(places[0].geometry.location);
 
+        var markers = [];
+        // Listen for the event fired when the user selects a prediction and retrieve
+        // more details for that place.
+        searchBox.addListener('places_changed', function() {
+          var places = searchBox.getPlaces();
+
+          if (places.length == 0) {
+            return;
+          }
+
+          // Clear out the old markers.
+          markers.forEach(function(marker) {
+            marker.setMap(null);
+          });
+          markers = [];
+
+          // For each place, get the icon, name and location.
+          var bounds = new google.maps.LatLngBounds();
+          places.forEach(function(place) {
+            if (!place.geometry) {
+              console.log("Returned place contains no geometry");
+              return;
+            }
+            var icon = {
+              url: place.icon,
+              size: new google.maps.Size(71, 71),
+              origin: new google.maps.Point(0, 0),
+              anchor: new google.maps.Point(17, 34),
+              scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+              map: map,
+              icon: icon,
+              title: place.name,
+              position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+              // Only geocodes have viewport.
+              bounds.union(place.geometry.viewport);
+            } else {
+              bounds.extend(place.geometry.location);
+            }
+          });
+          map.fitBounds(bounds);
         });
     }
     $(document).ready(function(){
-        $('#pac-input').click(function(){
-            $('#map').show();
-            initAutocomplete();
-        });
+        
+        $('#map').show();
+        initAutocomplete();
         
     });
     $('.listUser').sortable({
@@ -154,10 +198,14 @@ if(isset($_GET["id"]) && is_numeric($_GET["id"]) && $_GET["id"] != 0){
                     </label>
                     <label>
                         <span>Mapa:</span>
-                        <input id="pac-input" class="controls" type="text" placeholder="Search Box">
+                        <input id="mapa" type="text" value="<?php echo $that['mapa']; ?>" placeholder="">
                         <div class="mensaje"></div>
                     </label>
-                    <div id="map" style="height: 400px; display: none; margin-right: 10%"></div>
+                    <label>
+                        <input id="pac-input" class="controls" type="text" placeholder="Search Box">
+                        <div id="map" style="height: 400px; display: none; margin-right: 10%"></div>
+                    </label>
+                    
                     <label>
                         <span>Superficie &uacute;til:</span>
                         <input id="supercifie_util" type="text" value="<?php echo $that['supercifie_util']; ?>" require="" placeholder="60" />
