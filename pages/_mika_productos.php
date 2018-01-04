5dce818331f177a8c030a1179be0eb2541b4b918
jhygt46 <infolifeoperacional@gmail.com>
2018-01-04 20:34:32 -0300

    aa

--- pages/_mika_productos.php
+++ pages/_mika_productos.php
@@ -13,23 +13,16 @@ $admin = new Admin();
 //$admin->seguridad(1);
 
 /* CONFIG PAGE */
+$titulo = "Productos";
+$titulo_list = "Lista de Productos";
+$sub_titulo1 = "Ingresar Producto";
+$sub_titulo2 = "Modificar Modificar Producto";
+$accion = "_mika_crear_producto";
 
-//$res = $admin->arbol_categoria();
-$aux = $admin->con->sql("SELECT * FROM _mika_categorias WHERE id_page='".$_SESSION['user']['info']['id_page']."' AND eliminado='0'");
-
-$list = $aux['resultado'];
-$titulo = "Categorias";
-$titulo_list = "Lista de Categorias";
-$sub_titulo1 = "Ingresar Categoria";
-$sub_titulo2 = "Modificar Categoria";
-$accion = "_mika_crear_categoria";
-
-$eliminaraccion = "_mika_eliminar_categoria";
-$id_list = "id_cat";
-$eliminarobjeto = "Categoria";
-$page_mod = "pages/_mika_crear_categoria.php";
-$page_env = "pages/_mika_envoltura.php";
-$page_pro = "pages/_mika_productos.php";
+$eliminaraccion = "_mika_eliminar_producto";
+$id_list = "id_pro";
+$eliminarobjeto = "Producto";
+$page_mod = "pages/_mika_crear_producto.php";
 /* CONFIG PAGE */
 
 $id = 0;
@@ -38,39 +31,20 @@ if(isset($_GET["id"]) && is_numeric($_GET["id"]) && $_GET["id"] != 0){
     
     $id = $_GET["id"];
     $sub_titulo = $sub_titulo2;
-    $mm = $admin->con->sql("SELECT * FROM _mika_categorias WHERE id_page='".$_SESSION['user']['info']['id_page']."' AND id_cat='".$id."' AND eliminado='0'");
-    $that = $mm['resultado'][0];
+    $mm = $admin->con->sql("SELECT * FROM _mika_productos WHERE id_page='".$_SESSION['user']['info']['id_page']."' AND id_cat='".$id."' AND eliminado='0'");
+    $list = $mm['resultado'];
     
+    if(isset($_GET["id_pro"]) && is_numeric($_GET["id_pro"]) && $_GET["id_pro"] != 0){
+        
+        $aux = $admin->con->sql("SELECT * FROM _mika_productos WHERE id_page='".$_SESSION['user']['info']['id_page']."' AND id_pro='".$id_pro."' AND eliminado='0'");
+        $that = $aux['resultado'][0];
+        $id_pro = $_GET["id_pro"];
+    
+    }
 }
 
 
 ?>
-
-<script>
-    $('.listUser').sortable({
-        stop: function(e, ui){
-            var order = [];
-            $(this).find('.user').each(function(){
-                order.push($(this).attr('rel'));
-            });
-            var send = {accion: 'ordercat', values: order, tabla: '_category_mika', id: 'id_cat'};
-            $.ajax({
-                url: "ajax/index.php",
-                type: "POST",
-                data: send,
-                success: function(data){
-
-                }, error: function(e){
-
-                }
-            });
-            
-        }
-    });
-    $('.listUser').disableSelection();
-</script>   
-
-
 <div class="title">
     <h1><?php echo $titulo; ?></h1>
     <ul class="clearfix">
@@ -91,13 +65,18 @@ if(isset($_GET["id"]) && is_numeric($_GET["id"]) && $_GET["id"] != 0){
                     <input id="id" type="hidden" value="<?php echo $id; ?>" />
                     <input id="accion" type="hidden" value="<?php echo $accion; ?>" />
                     <label>
+                        <span>Numero:</span>
+                        <input id="numero" type="text" value="<?php echo $that['numero']; ?>" require="" placeholder="1" />
+                        <div class="mensaje"></div>
+                    </label>
+                    <label>
                         <span>Nombre:</span>
-                        <input id="nombre" type="text" value="<?php echo $that['nombre']; ?>" require="" placeholder="California Rolls" />
+                        <input id="nombre" type="text" value="<?php echo $that['nombre']; ?>" require="" placeholder="Salmon, palta, queso crema" />
                         <div class="mensaje"></div>
                     </label>
                     <label>
                         <span>Precio:</span>
-                        <input id="precio" type="text" value="<?php echo $that['precio']; ?>" require="" placeholder="3800" />
+                        <input id="precio" type="text" value="<?php echo $that['precio']; ?>" require="" placeholder="0" />
                         <div class="mensaje"></div>
                     </label>
                     <label style='margin-top:20px'>
