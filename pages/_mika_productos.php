d3077bde6838a225f772a183a717656fb4aa3849
jhygt46 <infolifeoperacional@gmail.com>
2018-01-04 23:14:54 -0300

    aa

--- pages/_mika_productos.php
+++ pages/_mika_productos.php
@@ -114,7 +114,7 @@ if(isset($_GET["id"]) && is_numeric($_GET["id"]) && $_GET["id"] != 0){
                     <ul class="clearfix">
                         <li class="nombre"><?php echo $numero; ?>.- <?php echo $nombre; ?></li>
                         <a title="Eliminar" class="icn borrar" onclick="eliminar('<?php echo $eliminaraccion; ?>', <?php echo $id; ?>, '<?php echo $eliminarobjeto; ?>', '<?php echo $nombre; ?>')"></a>
-                        <a title="Modificar" class="icn modificar" onclick="navlink('<?php echo $page_mod; ?>?id=<?php echo $id; ?>&id_pro=id=<?php echo $id; ?>')"></a>
+                        <a title="Modificar" class="icn modificar" onclick="navlink('<?php echo $page_mod; ?>?id=<?php echo $id; ?>&id_pro=<?php echo $id; ?>')"></a>
                     </ul>
                 </li>
                 
